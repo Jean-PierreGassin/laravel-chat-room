0018a4dccbbe3a722a9c027d40814203ce12557e
Jean-Pierre <jeanpierre.gassin@gmail.com>
2016-05-16 09:35:04 +1000

    add basic layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,31 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Chat Room</title>
+	<link rel="stylesheet" href="css/app.css"/>
+</head>
+<body>
+	<div class="row">
+		<div class="columns">
+			<h5>Who's online?</h5>
+		</div>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+		<div class="medium-8 columns">
+			<div class="message-area">
+				test
+			</div>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
+			<form>
+			  <div class="row">
+			    <div class="input-group">
+			        <textarea placeholder="Start chattin'..."></textarea>
+					<input type="submit" class="button" value="Send">
+			    </div>
+			  </div>
+			</form>
+		</div>
+	</div>
+</body>
+<script src="js/app.js"/>
 </html>
