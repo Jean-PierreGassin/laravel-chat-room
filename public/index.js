3b056ccd8f0595f52a9bd985712330dd961edf55
Jean-Pierre Gassin <jeanpierre.gassin@xcommedia.com.au>
2016-06-10 10:54:37 +1000

    Add local jQuery, basic functionality enabled

--- public/index.js
+++ public/index.js
@@ -3,13 +3,19 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/scripts', function(req, res){
+	res.sendFile(__dirname + '/jquery-3.0.0.min.js');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
