d84757332f7ed4475fa188387f6da67f4b2602c4
Jean-Pierre Gassin <jeanpierre.gassin@xcommedia.com.au>
2016-06-10 16:18:58 +1000

    Add online feature, add connection prompts and validation

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var clients = [];
+
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -11,8 +13,41 @@ app.get('/scripts', function(req, res){
 });
 
 io.on('connection', function(socket){
+	socket.on('join', function (name) {
+			socket.name = name;
+			clients.push(name);
+
+			console.log(socket.name + ' joined the chat.');
+	});
+
+	socket.on('user connected', function(msg){
+		var message = {
+			connected: msg + ' has connected.',
+			clients: clients
+		};
+
+		io.emit('user connected', message);
+	});
+
+	socket.on('typing', function(msg){
+		var message = msg  + ' is currently typing...';
+
+		socket.broadcast.emit('typing', message);
+	});
+
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
+	});
+
+	socket.on('disconnect', function(){
+		clients.splice(clients.indexOf(socket.name), 1);
+
+		var message = {
+			user: socket.name,
+			clients: clients
+		}
+
+		io.emit('disconnect', message);
 	});
 });
 
