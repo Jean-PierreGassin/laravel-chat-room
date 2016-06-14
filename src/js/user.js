b225f6685b2a19c21faa4fca16a4edb56e94dd29
Jean-Pierre Gassin <jeanpierre.gassin@xcommedia.com.au>
2016-06-14 16:45:35 +1000

    Fix bug in Firefox and Safari not setting users name

--- src/js/user.js
+++ src/js/user.js
@@ -1,27 +1,15 @@
 export class User {
 	// If the client is a Firefox user let everyone know
 	constructor() {
-		this.isFirefox = typeof InstallTrigger !== 'undefined';
-
-		if (this.isFirefox) {
-			this.user = 'Firefox User';
-		}
+		this.user = localStorage.getItem('user');
 	}
 
 	// Ask for the clients name
 	promptName() {
-		if (localStorage.user) {
-			this.user = localStorage.user;
-
-			return this.user;
-		}
-
-		if (!this.isFirefox) {
-			do {
-				this.user = prompt('What\'s your name?');
-				localStorage.user = this.user;
-			} while (this.user === null || this.user.length <= 1);
-		}
+		do {
+			this.user = prompt('What\'s your name?');
+			localStorage.setItem('user', this.user);
+		} while (this.user === null || this.user.length <= 1);
 
 		this.user.trim();
 
